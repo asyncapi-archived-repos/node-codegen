881bb59a62361e9c20453de40cf61bc85c4e2d1f
fmvilas <fmvilas@gmail.com>
2017-04-23 20:31:53 +0200

    Add services documentation with message details

--- templates/src/api/services/___service.js
+++ templates/src/api/services/___service.js
@@ -2,26 +2,46 @@ const {{service}} = module.exports = {};
 
 {{#each asyncapi.topics as |topic topicName|}}
   {{#if topic.publish}}
-    {{#if topic.publish.descriptionLines}}
 /**
+ {{#if topic.publish.descriptionLines}}
  {{#each topic.publish.descriptionLines}}
  * {{this}}
  {{/each}}
+ *
+ {{/if}}
+ * @param {Object} options
+ * @param {Object} options.message
+ {{#if topic.publish.headers}}
+ {{#each topic.publish.headers as |field fieldName|}}
+{{{docline field fieldName 'options.message.headers'}}}
+ {{/each}}
+ {{/if}}
+ {{#each topic.publish.payload as |field fieldName|}}
+{{{docline field fieldName 'options.message.payload'}}}
+ {{/each}}
  */
-    {{/if}}
 {{topic.serviceName}}.{{topic.publish.operationId}} = async ({message}) => {
   // Implement your business logic here...
 };
 
   {{/if}}
   {{#if topic.subscribe}}
-    {{#if topic.subscribe.descriptionLines}}
 /**
+ {{#if topic.subscribe.descriptionLines}}
  {{#each topic.subscribe.descriptionLines}}
  * {{this}}
  {{/each}}
+ *
+ {{/if}}
+ * @param {Object} options
+ * @param {Object} options.message
+ {{#each topic.subscribe.headers as |field fieldName|}}
+{{{docline field fieldName 'options.message.headers'}}}
+ {{/each}}
+ {{#each topic.subscribe.payload as |field fieldName|}}
+{{{docline field fieldName 'options.message.payload'}}}
+ {{/each}}
  */
-    {{/if}}
 {{topic.serviceName}}.{{topic.subscribe.operationId}} = async ({message}) => {
   // Implement your business logic here...
 };
